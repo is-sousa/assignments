1f861884b3211cbc660342e517ce07831b04a241
Ines Sousa <is.sousa@campus.fct.unl.pt>
2023-11-01 23:08:26 +0000

    Refactor code into three functions

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -2,26 +2,31 @@ import argparse
 import pandas as pd
 import numpy as np
 
-def clean_data(country):
+def load_data():
     data = pd.read_csv('life_expectancy/data/eu_life_expectancy_raw.tsv', sep='\t')
+    return data
 
+def clean_data(data):
     data[['unit', 'sex', 'age', 'region']] = data[r'unit,sex,age,geo\time'].str.split(
         ',', expand=True)
 
     data.drop(columns=data.columns[0], inplace=True)
 
-    melted_df = pd.melt(data, id_vars=['unit', 'sex', 'age', 'region'], var_name='year',
+    cleaned_data = pd.melt(data, id_vars=['unit', 'sex', 'age', 'region'], var_name='year',
                         value_name='value')
 
-    melted_df['value'] = melted_df['value'].str.replace(r'[^0-9.]', '', regex=True)
+    cleaned_data['value'] = cleaned_data['value'].str.replace(r'[^0-9.]', '', regex=True)
 
-    melted_df['value'] = melted_df['value'].replace('', np.nan)
+    cleaned_data['value'] = cleaned_data['value'].replace('', np.nan)
 
-    melted_df['year'] = melted_df['year'].astype(int)
+    cleaned_data['year'] = cleaned_data['year'].astype(int)
 
-    melted_df['value'] = melted_df['value'].astype(float)
+    cleaned_data['value'] = cleaned_data['value'].astype(float)
 
-    portugal_data = melted_df[melted_df['region'] == country]
+    return cleaned_data
+
+def save_data(cleaned_data, country):
+    portugal_data = cleaned_data[cleaned_data['region'] == country]
 
     portugal_data = portugal_data.dropna(subset=['value'])
 
@@ -32,7 +37,9 @@ def main():
     parser.add_argument('--country', default='PT')
     args = parser.parse_args()
 
-    clean_data(args.country)
+    data = load_data()
+    cleaned_data = clean_data(data)
+    save_data(cleaned_data, args.country)
 
 if __name__ == '__main__':  # pragma: no cover
     main()
--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -1,13 +1,16 @@
 """Tests for the cleaning module"""
 import pandas as pd
 
-from life_expectancy.cleaning import clean_data
+from life_expectancy.cleaning import load_data, clean_data, save_data
 from . import OUTPUT_DIR
 
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data("PT")
+    data = load_data()
+    cleaned_data = clean_data(data)
+    save_data(cleaned_data, "PT")
+
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
     )
