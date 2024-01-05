9b4d5e6b3b924de590205b1d0fb9d35d5dbe384e
Ines Sousa <is.sousa@campus.fct.unl.pt>
2024-01-05 22:32:55 +0000

    Enum

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -1,18 +1,84 @@
-import argparse
+from enum import Enum
 import pandas as pd
 import numpy as np
 
-def load_data():
-    data = pd.read_csv('life_expectancy/data/eu_life_expectancy_raw.tsv', sep='\t')
+class Region(Enum):
+    PT = 'PT'
+    BE = 'BE'
+    BG = 'BG'
+    AT = 'AT'
+    CH = 'CH'
+    CY = 'CY'
+    CZ = 'CZ'
+    DK = 'DK'
+    EE = 'EE'
+    EL = 'EL'
+    ES = 'ES'
+    EU = 'EU'
+    FI = 'FI'
+    FR = 'FR'
+    HR = 'HR'
+    HU = 'HU'
+    IS = 'IS'
+    IT = 'IT'
+    LI = 'LI'
+    LT = 'LT'
+    LU = 'LU'
+    LV = 'LV'
+    MT = 'MT'
+    NL = 'NL'
+    NO = 'NO'
+    PL = 'PL'
+    RO = 'RO'
+    SE = 'SE'
+    SI = 'SI'
+    SK = 'SK'
+    DE = 'DE'
+    AL = 'AL'
+    EA = 'EA'
+    EF = 'EF'
+    IE = 'IE'
+    ME = 'ME'
+    MK = 'MK'
+    RS = 'RS'
+    AM = 'AM'
+    AZ = 'AZ'
+    GE = 'GE'
+    TR = 'TR'
+    UA = 'UA'
+    BY = 'BY'
+    UK = 'UK'
+    XK = 'XK'
+    FX = 'FX'
+    MD = 'MD'
+    SM = 'SM'
+    RU = 'RU'
+    EA18 = 'EA18'
+    EA19 = 'EA19'
+    EFTA = 'EFTA'
+    EEA30_2007 = 'EEA30_2007'
+    EEA31 = 'EEA31'
+    EU27_2007 = 'EU27_2007'
+    EU28 = 'EU28'
+
+    @classmethod
+    def get_actual_countries(cls):
+        return [region.value for region in cls if len(region.value) == 2]
+
+def load_data(file_path='life_expectancy/data/eu_life_expectancy_raw.tsv'):
+    data = pd.read_csv(file_path, sep='\t')
+
     return data
 
-def clean_data(data, country):
-    data[['unit', 'sex', 'age', 'region']] = data[r'unit,sex,age,geo\time'].str.split(
+def clean_data(data, country: Region):
+    df = data.copy()
+
+    df[['unit', 'sex', 'age', 'region']] = df[df.columns[0]].str.split(
         ',', expand=True)
 
-    data.drop(columns=data.columns[0], inplace=True)
+    df.drop(columns=df.columns[0], inplace=True)
 
-    cleaned_data = pd.melt(data, id_vars=['unit', 'sex', 'age', 'region'], var_name='year',
+    cleaned_data = pd.melt(df, id_vars=['unit', 'sex', 'age', 'region'], var_name='year',
                         value_name='value')
 
     cleaned_data['value'] = cleaned_data['value'].str.replace(r'[^0-9.]', '', regex=True)
@@ -23,20 +89,23 @@ def clean_data(data, country):
 
     cleaned_data['value'] = cleaned_data['value'].astype(float)
 
-    portugal_data = cleaned_data[cleaned_data['region'] == country]
+    country_data = cleaned_data[cleaned_data['region'] == country.value]
 
-    portugal_data = portugal_data.dropna(subset=['value'])
+    country_data = country_data.dropna(subset=['value'])
 
-    return portugal_data
+    return country_data
 
-def save_data(portugal_data):
-    portugal_data.to_csv('life_expectancy/data/pt_life_expectancy.csv', index=False)
+def save_data(country_data, file_path='life_expectancy/data/country_life_expectancy.csv'):
+    country_data.to_csv(file_path, index=False)
 
 def main():
     data = load_data()
-    cleaned_data = clean_data(data, 'PT')
-    save_data(cleaned_data)
-    return clean_data
+    countries = Region.get_actual_countries()
+
+    for country in countries:
+        cleaned_data = clean_data(data, Region[country])
+        save_data(cleaned_data)
+        return cleaned_data
 
 if __name__ == '__main__':  # pragma: no cover
     main()
